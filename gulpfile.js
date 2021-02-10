bfcf0e4bebcb252211eb043243c231789524d8de
Aleksandr <alkov0312@gmail.com>
2021-02-10 09:20:22 +0300

    0

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,6 @@ const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const wait = require("gulp-wait");
-const cleancss = require("gulp-clean-css");
-const concat = require("gulp-concat");
-const uglify = require("gulp-uglify-es").default;
-const autoprefixer = require("gulp-autoprefixer");
-
-const libs = {
-    css: [],
-    js: [],
-};
 
 const server = () => {
     browserSync.init({
@@ -31,39 +22,12 @@ const scss = () =>
         .pipe(dest("src/data"))
         .pipe(browserSync.stream());
 
-const scriptsBuild = () =>
-    src(libs.js.push("src/data/common.js"))
-        .pipe(concat("common.min.js"))
-        .pipe(uglify())
-        .pipe(dest("build/data/"));
-
-const stylesBuild = () =>
-    src(libs.css.push("src/data/common.css"))
-        .pipe(concat("common.min.css"))
-        .pipe(
-            autoprefixer({
-                overrideBrowserslist: ["last 10 versions"],
-                grid: true,
-            })
-        )
-        .pipe(
-            cleancss({
-                level: { 1: { specialComments: 0 } },
-            })
-        )
-        .pipe(dest("build/data/"));
-
-const imagesOptimization = () => {};
-
-const copyFiles = () => {};
-
 const watching = () => {
     watch("src/data/scss/**/*.scss").on("change", scss);
-    watch("src/css/**/*.css").on("change", browserSync.reload);
     watch("src/**/*.html").on("change", browserSync.reload);
+    watch("src/**/*.css").on("change", browserSync.reload);
+    watch("src/**/*.js").on("change", browserSync.reload);
+    watch("src/data/json/**/*.json").on("change", browserSync.reload);
 };
 
-const build = () => {};
-
 exports.default = parallel(server, scss, watching);
-exports.stylesBuild = stylesBuild;
